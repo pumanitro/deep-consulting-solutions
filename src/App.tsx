afdf83f43e2050a4b8c8a76ff6e6f4738e13dee4
pumanitro <patrykjanik1710@gmail.com>
2020-11-29 17:06:02 +0100

    Added routing functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {Home} from "./pages/Home/Home";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {CityDetail} from "./pages/CityDetail/CityDetail";
 
 function App() {
   return (
-      <Home/>
+      <Router>
+        <Switch>
+          <Route path="/city-detail/:cityName">
+            <CityDetail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
   );
 }
 
