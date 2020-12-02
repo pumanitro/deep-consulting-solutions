a2aa6b4dcced3083bad324180a806142b5a54f52
pumanitro <patrykjanik1710@gmail.com>
2020-12-02 19:51:59 +0100

    Added numbering to top 15 cities.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -18,14 +18,15 @@ export const Cities : FC<CitiesPropsType> = ({topCities}) => {
         setCities(cities.filter((country: any) => country.location.name !== name));
     }
 
-    const cityComponents = _.sortBy(cities, 'location.name').map((city: any) => <>
-        <Link to={CITY_DETAIL(city.location.name)}><span>{city.location.name} {city.current.temperature}°C</span></Link>
+    const cityComponents = _.sortBy(cities, 'location.name').map((city: any, index: number) => <>
+        <Link to={CITY_DETAIL(city.location.name)}><span>{index + 1}. {city.location.name} {city.current.temperature}°C</span></Link>
         <button onClick={() => removeCountry(city.location.name)}>Remove</button>
     </>)
 
     return (
         <>
             <FavoritesList />
+            <h5>Top 15 cities by population</h5>
             <CityList cities={cityComponents} />
         </>
     );
